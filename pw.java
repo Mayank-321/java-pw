6d204bd9be3e34990559d571e91e91b352daddaf
Mayank-321 <tiwarimayank192002@gmail.com>
2024-08-07 17:51:03 +0530

    modified pw.java added m2 & m3 method

--- pw.java
+++ pw.java
@@ -4,4 +4,13 @@ class pw
         { 
           \\code 
           } 
+    public void m2()
+        { 
+          \\code 
+          } 
+    public void m3()
+        { 
+          \\code 
+          } 
+
 }
